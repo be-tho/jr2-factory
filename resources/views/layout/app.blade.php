162bf50dfa51f8025021db3d856e29066253b372
beto <56642267+be-tho@users.noreply.github.com>
2024-05-23 21:38:30 -0300

    Route linkeado con la vista del home

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('app.js')
+    <title>JR2 | @yield('title', 'Bienvenido')</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Acerca de</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
---
+++ resources/views/sections/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+@section('title', 'Home')
+@section('content')
+    <section>
+        <h1>Home</h1>
+        <p>Bienvenido a la página de inicio</p>
+    </section>
+@endsection
+
